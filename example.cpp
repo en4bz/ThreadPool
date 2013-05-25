8407c93d8561b7795ad212038027b267cb3d2b70
Ian Forbes <ian.forbes@mail.mcgill.ca>
2013-05-25 00:30:27 -0300

    Fixed example.cpp as it wouldn't compile in previous commit.

--- example.cpp
+++ example.cpp
@@ -5,12 +5,12 @@
 #include "ThreadPool.hpp"
 
 int main(void){
-    ThreadPool<PRIORITY_POLICY> pool(1);
+    ThreadPool<PRIORITY_POLICY> pool(2);
     std::vector< std::future<int> > results;
 
-    for(int i = 0; i < 8; ++i) {
+    for(int i = 0; i < 11; i++) {
         results.push_back(
-            pool.enqueue([i] {
+            pool.enqueue(i, [i] {
                 std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " << i << std::endl;
@@ -19,9 +19,9 @@ int main(void){
         );
     }
 
-    for(size_t i = 0;i<results.size();++i)
+    for(size_t i = 0; i < results.size(); i++)
         std::cout << results[i].get() << ' ';
-    std::cout << std::endl;
 
+    std::cout << std::endl;
     return 0;
 }
